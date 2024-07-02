7de0fa42eb576f0cf7fc8f55d547165d7b1e2c6a
citrafadilah <citrafadilahhasibuan_2226240089@mhs.mdp.ac.id>
2024-07-02 17:32:56 +0700

    add vercel

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\API\ObatController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::get('obat',[ObatController::class, 'index']);
+Route::post('obat',[ObatController::class, 'store']);
